86b6c9589913fd2470a49ddbff5bb9de06399aa2
Ahmad-17R <ahmadhoney7850@gmail.com>
2026-08-11 14:33:43 +0500

    check link temp fix

--- src/customer/pages/CheckoutPage.tsx
+++ src/customer/pages/CheckoutPage.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { useCart } from '../../context/CartContext'
 
 const EMIRATES = ['Abu Dhabi', 'Dubai', 'Sharjah', 'Ajman', 'Umm Al Quwain', 'Ras Al Khaimah', 'Fujairah']
